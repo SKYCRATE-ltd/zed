a7fd7bcae91df29340c7ea0f5420de306c618a16
dumpstergoon <graham_daniel_robertson@protonmail.com>
2021-04-10 14:29:34 +0100

    fix?

--- index.js
+++ index.js
@@ -247,7 +247,7 @@ export const TypeDescriptor = (
 	.static(statics);
 };
 
-export default function Type(...args) {
+export function Type(...args) {
 	if (is.global(this)) {
 		if (!is.string(args[0]))
 			return Type('Type', format.model, ...args);
